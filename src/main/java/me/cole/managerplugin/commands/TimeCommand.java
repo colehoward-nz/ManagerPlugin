1411db77d72374ba743ccd1497c4e99b8d61092e
Cole <74393866+colehoward-nz@users.noreply.github.com>
2023-09-20 18:37:26 +1200

    completed timecommand

--- src/main/java/me/cole/managerplugin/commands/TimeCommand.java
+++ src/main/java/me/cole/managerplugin/commands/TimeCommand.java
@@ -8,8 +8,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.sql.Time;
-
 public class TimeCommand implements CommandExecutor {
     private final Manager plugin;
     private final String noPerms;
@@ -25,16 +23,31 @@ public class TimeCommand implements CommandExecutor {
             World world = player.getWorld();
 
             if (player.hasPermission("manager.time")) {
+                int time = 0;
+                if (args.length > 0) {
+                    try {
+                        time = Integer.parseInt(args[0]);
+                    } catch (Exception ignored) {
+                    }
+                }
                 if (args.length == 0) {
                     player.sendMessage(ChatColor.RED + "Incorrect usage: /time <day|night|time>");
                 }
-                else if (args[0].equalsIgnoreCase("day")){
+
+                else if (args[0].equals("day")){
                     world.setTime(0);
                     player.sendMessage(ChatColor.YELLOW + "Time set to DAY in your world.");
                 }
-                else if (args[0].equalsIgnoreCase("night")){
-                    world.setTime();
-                    player.sendMessage(ChatColor.YELLOW + "Time set to DAY in your world.");
+                else if (args[0].equals("night")){
+                    world.setTime(13000);
+                    player.sendMessage(ChatColor.YELLOW + "Time set to NIGHT in your world.");
+                }
+                else if (time <= 20000 && time >= 0) {
+                    world.setTime(time);
+                    player.sendMessage(ChatColor.YELLOW + "Time set to " + time + " in your world.");
+                }
+                else {
+                    player.sendMessage(ChatColor.RED + "Incorrect usage: /time <day|night|time>");
                 }
             }
             else {
